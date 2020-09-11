41457f5028c9398c8c38eb8181bee91c88b5ff7c
Teerasej <teerasej@gmail.com>
2020-09-11 13:05:46 +0700

    prepare application builder

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using System.IO;
 
 namespace GraphAPI_2
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static readonly string _clientId = "38478b47-0715-4ce9-8d56-90ff0d503ce4";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+
+            string[] scopes = new string[] {
+                "User.Read"
+            };
+
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+
+        }   
     }
 }
